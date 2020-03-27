c81777fa0fda77d760e2e2328b7138ba23281fd8
emilychang <changemily5@gmail.com>
2020-03-27 16:55:34 +1000

    Billboard class skeleton

--- src/Billboard.java
+++ src/Billboard.java
@@ -1,8 +1,47 @@
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * Billboard class
+ */
+
 public class Billboard {
     String Billboard_name;
     String Billboard_text;
     String bg_colour;
     String image_file;
-    //DateTime time_scheduled;
-    //test
+    LocalDate time_scheduled;
+    Duration duration;
+
+    public void Billboard(String billboard_name, String text, String bg_colour)
+    {
+
+    }
+
+    public void Billboard(String billboard_name, String text, String bg_colour, String image_file , LocalDate time_schedule, Duration duration)
+    {
+
+    }
+
+    public void Set_text(String text)
+    {
+
+    }
+
+    public void Bg_colour(String colour)
+    {
+
+    }
+
+    public void Add_image(String file_name)
+    {
+
+    }
+
+    public void Schedule_billboard(String name, Duration duration, LocalDate time_schedule)
+    {
+
+    }
+
 }
