658c3ef28d4ed18d201a5b6a31560f830b4e40b4
emilychang <changemily5@gmail.com>
2020-04-06 14:01:35 +1000

    BillboardTest - Tests implemented
    Billboard Class
    - edited
    - bg_colour and Add_image not complete - exceptions need to be added

--- src/BillboardTest.java
+++ src/BillboardTest.java
@@ -1,15 +1,69 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class BillboardTest {
 
-// Test 1: Create Billboard object
-// Test 1.2: Create Billboard object with scheduled time
-// Test 2: Create Billboard object - invalid params
-// Test 2.2: Create Billboard object with scheduled time- invalid params
-// Test 3: Set text - invalid params
-// Test 4: Set bg colour - colour does not exist
-// Test 4.2: Set bg colour - invalid param
-// Test 5: Set image - invalid params
-// Test 5.2: Set image - file does not exist
-// Test 6: Schedule_billboard - invalid params
-// Test 6.2: Schedule_billboard - invalid time (time in the past)
-// Test 7: Schedule_billboard - Negative duration
+    Billboard billboard;
+
+    // Test 1: Constructing a Billboard object without with scheduled time
+    @BeforeEach @Test
+    public void setUpBillboard() {
+        billboard = new Billboard("Billboard 1", "Hello World", "blue", "image.jpg");
+    }
+
+    @Test
+    // Test 1.2: Constructing a Billboard object with scheduled time
+    public void setUpBillboardTime() {
+        Billboard billboard2 = new Billboard("Billboard 2", "Hello World", "red", "image.jpg",
+                LocalDate.parse("2020-04-06"),5);
+    }
+
+    @Test
+    // Test 1.3: Constructing a Billboard object without image
+    public void setUpBillboard_img() {
+        Billboard billboard3 = new Billboard("Billboard 2", "Hello World", "red", "No Image",
+                LocalDate.parse("2020-04-06"),5);
+    }
+
+    @Test
+    // Test 2: Set bg colour - colour does not exist
+    public void set_bgColour() throws Exception{
+        assertThrows(Exception.class, () -> {
+            billboard.Bg_colour("girl");
+        });
+    }
+    // Test 3: Set image - file does not exist
+    @Test
+    public void set_image() throws Exception{
+        assertThrows(Exception.class, () -> {
+            billboard.Add_image("nonexistent");
+        });
+    }
+
+    // Test 4: Schedule_billboard - invalid time (time in the past)
+    @Test
+    public void Schedule_billboard() throws Exception{
+        assertThrows(Exception.class, () -> {
+            billboard.Schedule_billboard(5, LocalDate.parse("2000-11-01"));
+        });
+    }
+    // Test 5: Schedule_billboard - Negative duration
+    @Test
+    public void Negative_duration() throws Exception{
+        assertThrows(Exception.class, () -> {
+            billboard.Schedule_billboard(-1, LocalDate.parse("2000-11-01"));
+        });
+    }
+
+    // Test 5: Schedule_billboard - Zero duration
+    @Test
+    public void Zero_duration() throws Exception{
+        assertThrows(Exception.class, () -> {
+            billboard.Schedule_billboard(0, LocalDate.parse("2000-11-01"));
+        });
+    }
 }
--- src/billboardManager.java
+++ src/billboardManager.java
@@ -1,51 +1,51 @@
-import java.time.Duration;
-import java.time.LocalDate;
-
-/**
- * Billboard Manager class
- * Class contains methods for creating and controlling billboards.
- * @author - Jarod Evans
- * @version - skeleton
- */
-
-public class billboardManager {
-    /**
-     * Method to create and edit billboards
-     * Parameters -
-     * @param billboard_name
-     * @param text
-     * @param bg_colour
-     * @param image (optional)
-     * @param schedule time (optional):void
-     * @param durationMinutes
-     */
-    public void Create_edit_Billboard (String billboard_name, String text, String bg_colour, String image,
-    localDate schedule time, Duration durationMinutes){
-
-    }
-
-
-
-    /**
-    * Method for listing current billboards
-    * Return type void
-    */
-    public void List_Billboards(){
-
-    }
-
-
-
-    /**
-     * Method for getting billboard information
-     * @param billboard_name
-     * Return type void
-     */
-    public void Get_billboard_info(String billboard_name){
-
-    }
-
-
-}
-
-
+//import java.time.Duration;
+//import java.time.LocalDate;
+//
+///**
+// * Billboard Manager class
+// * Class contains methods for creating and controlling billboards.
+// * @author - Jarod Evans & Emily Chang
+// * @version - skeleton
+// */
+//
+//public class billboardManager {
+//    /**
+//     * Method to create and edit billboards
+//     * Parameters -
+//     * @param billboard_name
+//     * @param text
+//     * @param bg_colour
+//     * @param image (optional)
+//     * @param schedule time (optional):void
+//     * @param durationMinutes
+//     */
+//    public void Create_edit_Billboard (String billboard_name, String text, String bg_colour, String image,
+//    localDate schedule time, Duration durationMinutes){
+//
+//    }
+//
+//
+//
+//    /**
+//    * Method for listing current billboards
+//    * Return type void
+//    */
+//    public void List_Billboards(){
+//
+//    }
+//
+//
+//
+//    /**
+//     * Method for getting billboard information
+//     * @param billboard_name
+//     * Return type void
+//     */
+//    public void Get_billboard_info(String billboard_name){
+//
+//    }
+//
+//
+//}
+//
+//
