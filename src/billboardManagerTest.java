49bad07ebecd441b304877b3ce7316d6213f7d9d
Jarod <jarodevans98@gmail.com>
2020-04-08 20:27:32 +1000

    Created billboardManagerTest skeleton for the purpose of Test driven development.

---
+++ src/billboardManagerTest.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+/**
+ * This class contains methods that test the functionality of billboardManager class methods
+ *  * @author — Jarod Evans
+ *  * @version - skeleton
+ */
+
+public class billboardManagerTest{
+
+    //Test 1: Checks if a billboard can be created and/or edited.
+    @Test
+    public static void Create_edit_Billboard(){
+
+    }
+
+    //Test 2: Checks if all current billboards can be listed.
+    @Test
+    public static void List_Billboards(){
+
+    }
+
+    //Test 3: Checks if all information pertaining to a specified billboard can be obtained.
+    @Test
+    public static void Get_billboard_info(String billboard_name){
+
+    }
+
+    //Test 4: Checks if the billboard schedule can be viewed correctly.
+    @Test
+    private static void View_schedule()
+    {
+
+    }
+
+    //Test 5: Checks if a specified billboard can be deleted.
+    @Test
+    private static void Delete_billboard(String billboard_name)
+    {
+
+    }
+
+    //Test 6: Checks if a billboard can be schduled for a timed removal.
+    @Test
+    private static void Schedule_Remove_billboard(String billboard_name)
+    {
+
+
+    }
+
+}
