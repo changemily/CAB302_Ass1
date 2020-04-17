1facae00adeea7f3172d34bd4bf4f00fee8d8479
Harry <harry.estreich1@gmail.com>
2020-04-17 14:37:06 +1000

    Added skeleton for the userManagerTest class, for half of class,

---
+++ src/userManagerTest.java
@@ -0,0 +1,101 @@
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class contains methods that test the functionality of billboardManager class methods
+ *  * @author — Harry Estreich
+ *  * @version - skeleton
+ */
+
+public class userManagerTest{
+
+    // Remove one permission
+    @Test
+    public static void remove_a_permission(){
+
+    }
+
+    // Remove multiple permissions
+    @Test
+    public static void remove_permissions(){
+
+    }
+
+    // Add one permission
+    @Test
+    public static void add_a_permission(){
+
+    }
+
+    // Add multiple permissions
+    @Test
+    public static void add_permissions(){
+
+    }
+
+    // Add a permission that user already has
+    @Test
+    public static void add_existing_permission(){
+
+    }
+
+    // Remove edit user for self user -> fails
+    @Test
+    public static void remove_self_user(){
+
+    }
+
+    // Fail to edit permissions without edit user permission
+    @Test
+    public static void edit_user_without_admin(){
+
+    }
+
+    // Set own password, with admin, pass
+    @Test
+    public static void edit_own_password_admin(){
+
+    }
+
+    // Set own password, without admin, pass
+    @Test
+    public static void edit_own_password_without_admin(){
+
+    }
+
+    // Set other password, with admin, pass
+    @Test
+    public static void edit_other_password_admin(){
+
+    }
+
+    // Set other password, without admin, fail
+    @Test
+    public static void edit_other_password_without_admin(){
+
+    }
+
+    // Delete own user, fail
+    @Test
+    public static void delete_own_user(){
+
+    }
+
+    // Delete other user, with admin, pass
+    @Test
+    public static void delete_other_user_admin(){
+
+    }
+
+    // Delete other user, without admin, fail
+    @Test
+    public static void delete_other_user_without_admin(){
+
+    }
+
+    // Log out
+    @Test
+    public static void log_out_test(){
+
+    }
+}
+
