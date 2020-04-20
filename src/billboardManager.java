1e728c11655f96333dc9b064e328a1fbb75a559d
emilychang <changemily5@gmail.com>
2020-04-20 15:48:23 +1000

    billboardManager Unit tests started - Created Mock HasMap for Billboards billboardManager Skeleton edited

--- src/billboardManager.java
+++ src/billboardManager.java
@@ -1,24 +1,18 @@
-/*
+
 import java.time.Duration;
 import java.time.LocalDate;
 
-*/
+
 /**
  * Billboard Manager class
  * Class contains methods for creating and controlling billboards.
  * @author - Jarod Evans & Emily Chang
- * @version - skeleton
- *//*
+ * @version - skeleton update
+ */
 
 
 public class billboardManager {
     //Variables
-    String Billboard_Name;
-    String Text;
-    String bg_Colour;
-    String Image;
-    localDate Schedule;
-    Duration Minutes;
     //Counter for the amount of billboards in circulation.
     int counter;
     //An array for storing billboard info.
@@ -26,108 +20,94 @@ public class billboardManager {
     String[] bb_info;
 
 
+    //Obtain_data - Extract serialized objects from database, deserialize and store in list of objects
 
-    */
+    //Store_data - Serialize objects in list and write to DB
+
 /**
      * Method to create and edit billboards
      * Parameters -
-     * @param billboard_name Billboards name
-     * @param text Text on the billboard
-     * @param bg_colour Background colour of the billboard
-     * @param image (optional)
-     * @param schedule time (optional):void
-     * @param durationMinutes The duration in minutes it will be displayed
-     *//*
+     *  @param billboard_name Billboard's name
+     * @param text Text set on Billboard
+     * @param bg_colour Background colour of Billboard
+     * @param image_file file name of image on Billboard
+     * @param time_scheduled Time (date) Billboard is scheduled for showing
+     * @param Duration_mins Duration (minutes) Billboard is displayed for
+     */
 
-    public void Create_edit_Billboard (String billboard_name, String text, String bg_colour, String image,
-    localDate schedule_time, Duration durationMinutes){
-        //Pass params to variables.
-        bb_info[0] = (this.Billboard_Name = billboard_name);
-        bb_info[1] = (this.Text = text);
-        bb_info[2] = (this.bg_Colour = bg_colour);
-        bb_info[3] = (this.Image = image);
-        bb_info[4] = ToString.((this.Schedule = schedule_time));
-        bb_info[5] = ToString.((this.Minutes = durationMinutes));
-        //Pass Params to array as info.
-        try{
-            //Pass users params to billboard creating class.
-            Billboard ToString.(Billboard_Name) = new Billboard(Billboard_Name, text,
-                    bg_Colour, Image, Schedule, Minutes);
-            //Update counter
-            counter++;
-            //Store billboard information in a 2D array.
-            for(int i = 0; i < counter; i++){
-                for(int j = 0; j < 6; j++){
-                    billboard_info[counter][i] = bb_info[j];
-                }
-            }
-        }catch{
-            //Error Message.
-            System.out.println("The billboard cannot be created.");
-        }
+    public static void Create_edit_Billboard (String billboard_name, String text, String bg_colour, String image_file,
+    LocalDate time_scheduled, int Duration_mins){
+
+        //Create billboard object
+        Billboard billboard  = new Billboard(billboard_name, text, bg_colour, image_file,
+                time_scheduled, Duration_mins);
+
+        //if Billboard exists
+            //edit billboard info
+
+        //if billboard does not exist
+            //Store Billboard info in Database.
     }
 
-
-
-    */
 /**
     * Method for listing current billboards
     * Return type void
-    *//*
+    */
 
-    public void List_Billboards(){
+    public static void List_Billboards(){
 
     }
 
-
-
-    */
 /**
-     * Method for getting billboard information
+     * Method for retrieving billboard information from database
      * @param billboard_name
      * Return type void
-     *//*
+     */
 
-    public void Get_billboard_info(String billboard_name){
-
+    public static void Get_billboard_info(String billboard_name){
+        //retrieve billboard info from database
     }
 
-    */
 /**
      * List of billboards that have been scheduled
-     *//*
+     */
 
-    private void View_schedule()
+    private static void View_schedule()
     {
         //returns list of Billboard objects from database
         // billboard's name, creator, time scheduled and duration
     }
 
-    */
 /**
      *
      * @param billboard_name Name of billboard being deleted
-     *//*
+     */
 
-    private void Delete_billboard(String billboard_name)
+    private static void Delete_billboard(String billboard_name)
     {
-        //remove billboard object from database
+        //remove billboard object from HashMap
     }
 
-    */
+
+    private static void Schedule_Billboard(int Duration_mins, LocalDate time_scheduled) throws Exception{
+        //edit schedule information of Billboard object
+        Schedule_Billboard(Duration_mins, time_scheduled);
+
+        //Add new viewing time and duration to HashMap
+    }
 /**
      *
      * @param billboard_name Name of billboard being removed from schedule
-     *//*
+     */
 
-    private void Schedule_Remove_billboard(String billboard_name)
+    private static void Schedule_Remove_billboard(String billboard_name)
     {
-        //remove scheduled time from billboard object
+        //remove scheduled time from billboard object in HashMap
+        //remove from schedule HashMap
 
     }
-
 
 }
 
 
-*/
+
--- src/billboardManagerTest.java
+++ src/billboardManagerTest.java
@@ -2,52 +2,91 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-
+import java.util.HashMap;
+import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 /**
  * This class contains methods that test the functionality of billboardManager class methods
- *  * @author — Jarod Evans
+ *  * @author — Jarod Evans & Emily Chang
  *  * @version - skeleton
  */
 
 public class billboardManagerTest{
 
-    //Test 1: Checks if a billboard can be created and/or edited.
+    HashMap<String, Billboard> billboardList;
+
+    //Create mock HashMap of billboards
+    @BeforeEach
+    public void Create_bbMap(){
+        //Create billboards to store in Billboard HashMap
+        Billboard Billboard_1 = new Billboard("Billboard_1", "hello world", "red", "image.jpg");
+        Billboard Billboard_2 = new Billboard("Billboard_2", "CAB302",
+                "yellow", "image.jpg", LocalDate.parse("20-04-2020"), 5);
+        Billboard Billboard_3 = new Billboard("Billboard_3", "goodbye",
+                "blue", "image.jpg", LocalDate.parse("30-01-2020"), 15);
+        Billboard Billboard_4 = new Billboard("Billboard_4", "test",
+                "green", "image.jpg");
+        Billboard Billboard_5 = new Billboard("Billboard_5", "hello",
+                "orange", "No Image");
+
+
+        billboardList = new HashMap<String, Billboard>();
+        //populate HashMap
+        billboardList.put(Billboard_1.Billboard_name, Billboard_1);
+        billboardList.put(Billboard_2.Billboard_name, Billboard_2);
+        billboardList.put(Billboard_3.Billboard_name, Billboard_3);
+        billboardList.put(Billboard_4.Billboard_name, Billboard_4);
+        billboardList.put(Billboard_5.Billboard_name, Billboard_5);
+    }
+
+    //Test 1: Checks if a billboard can be created and added to list.
     @Test
-    public static void Create_edit_Billboard(){
+    public void Create_Billboard(){
 
     }
 
-    //Test 2: Checks if all current billboards can be listed.
+    //Test 2: Checks if a billboard can be edited.
     @Test
-    public static void List_Billboards(){
+    public void Edit_Billboard(){
 
     }
 
-    //Test 3: Checks if all information pertaining to a specified billboard can be obtained.
+    //Test 3: Checks if all current billboards can be listed.
     @Test
-    public static void Get_billboard_info(String billboard_name){
+    public void List_Billboards(){
 
     }
 
-    //Test 4: Checks if the billboard schedule can be viewed correctly.
+    //Test 4: Checks if all information pertaining to a specified billboard can be obtained.
     @Test
-    private static void View_schedule()
+    public void Get_billboard_info(String billboard_name){
+
+    }
+
+    //Test 5: Checks if the billboard schedule can be viewed correctly.
+    @Test
+    private void View_schedule()
     {
 
     }
 
-    //Test 5: Checks if a specified billboard can be deleted.
+    //Test 6: Checks if a specified billboard can be deleted.
     @Test
-    private static void Delete_billboard(String billboard_name)
+    private void Delete_billboard(String billboard_name)
     {
 
     }
 
-    //Test 6: Checks if a billboard can be scheduled for a timed removal.
+    //Test 6: Schedule billboard in vacant time slot
+    //
+
+    //Test 6.2: Schedule billboard in non-vacant time slot - should take precedence over existing billboard scheduled
+    //
+
+    //Test 7: Checks if a billboard can be removed from schedule.
     @Test
-    private static void Schedule_Remove_billboard(String billboard_name)
+    private void Schedule_Remove_billboard(String billboard_name)
     {
 
 
