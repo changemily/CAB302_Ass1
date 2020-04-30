aa9257f681065ce7f478cb884bd8840896f2cefa
harryestreich <62687508+harryestreich@users.noreply.github.com>
2020-04-30 11:01:07 +1000

    Extended the skeleton for User Manager tests, each test is now set up, just waiting on further confirmation as to how check tests are passed, these User Manager are for the add_user_permissions, remove_user_permissions (separated set_user_permissions), set_user_password, get_user_password (new), and delete_user

--- src/TestUserManager.java
+++ src/TestUserManager.java
@@ -3,99 +3,180 @@ import org.junit.jupiter.api.Test;
 /**
  * This class contains methods that test the functionality of billboardManager class methods
  *  * @author — Harry Estreich
- *  * @version - skeleton
+ *  * @version - extended skeleton
  */
 
 public class TestUserManager {
 
-    // Remove one permission
+    // Test 1.1 Remove a permission from same user
     @Test
-    public static void remove_a_permission(){
-
+    public void remove_permission(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.remove_user_permissions(adminUser, "Edit Billboards");
+        // pass
     }
 
-    // Remove multiple permissions
+    // Test 1.2 Remove a permission from different user
     @Test
-    public static void remove_permissions(){
-
+    public void remove_permission_different(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.remove_user_permissions(otherUser, "Edit Billboards");
+        // pass
     }
 
-    // Add one permission
+    // Test 2.1 Remove permission that doesn't exist from same user
     @Test
-    public static void add_a_permission(){
-
+    public void remove_permission_non_exist(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.remove_user_permissions(adminUser, "Schedule Billboards");
+        // fail
     }
 
-    // Add multiple permissions
+    // Test 2.2 Remove permission that doesn't exist from different user
     @Test
-    public static void add_permissions(){
-
+    public void remove_permission_non_exist_different(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.remove_user_permissions(otherUser, "Schedule Billboards");
+        // fail
     }
 
-    // Add a permission that user already has
+    // Test 3.1 Remove permission from a user that doesn't exist
     @Test
-    public static void add_existing_permission(){
-
+    public void remove_permission_non_exist_user(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.remove_user_permissions(otherUser, "Schedule Billboards");
+        // fail
     }
 
-    // Remove edit user for self user -> fails
+    // Test 4.1 Add permission to own user with admin
     @Test
-    public static void remove_self_user(){
-
+    public void add_a_permission(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.add_user_permissions(adminUser, "Schedule Billboards");
+        // pass
     }
 
-    // Fail to edit permissions without edit user permission
+    // Test 4.2 Add a permission to own user without admin
     @Test
-    public static void edit_user_without_admin(){
-
+    public void add_a_permissions(){
+        User adminUser = new User("Admin", "1234", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.add_user_permissions(adminUser, "Schedule Billboards");
+        // fail
     }
 
-    // Set own password, with admin, pass
+    // Test 4.3 Add permission to another user with admin
     @Test
-    public static void edit_own_password_admin(){
-
+    public void add_a_permission_different(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.add_user_permissions(otherUser, "Schedule Billboards");
+        // pass
     }
 
-    // Set own password, without admin, pass
-    @Test
-    public static void edit_own_password_without_admin(){
+    // Test 4.4 Add permission to another user without admin
 
+    @Test
+    public void add_existing_permission(){
+        User adminUser = new User("Admin", "1234","Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.add_user_permissions(otherUser, "Schedule Billboards");
+        // fail
     }
 
-    // Set other password, with admin, pass
+    // Test 4.5 Add permission to own user that already exists
     @Test
-    public static void edit_other_password_admin(){
-
+    public void add_a_permission(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.add_user_permissions(adminUser, "Edit Billboards");
+        // fail
     }
 
-    // Set other password, without admin, fail
+    // Test 5.1 Remove edit user for self user -> fails
     @Test
-    public static void edit_other_password_without_admin(){
-
+    public void remove_self_user(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.remove_user_permissions(adminUser, "Edit Users");
+        // fail
     }
 
-    // Delete own user, fail
+    // Test 6.1 Set own password, with admin, pass
     @Test
-    public static void delete_own_user(){
-
+    public void edit_own_password_admin(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.set_user_password(adminUser, "4321");
+        // pass
     }
 
-    // Delete other user, with admin, pass
+    // Test 6.2 Set own password, without admin, pass
     @Test
-    public static void delete_other_user_admin(){
+    public void edit_own_password_without_admin(){
+        User adminUser = new User("Admin", "1234", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.set_user_password(adminUser, "4321");
+        // pass
+    }
 
+    // Test 6.3 Set other password, with admin, pass
+    @Test
+    public void edit_other_password_admin(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.set_user_password(otherUser, "8765");
+        // pass
+    }
+
+    // Test 6.4 Set other password, without admin, fail
+    @Test
+    public void edit_other_password_without_admin(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(otherUser, adminUser);
+        admin.set_user_password(adminUser, "4321");
+        // fail
+    }
+
+    // Test 7.1 Delete own user, fail
+    @Test
+    public void delete_own_user(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        userManager admin = new userManager(adminUser, adminUser);
+        admin.delete_user(adminUser);
+        // fail
+    }
+
+    // Test 7.2 Delete other user, with admin, pass
+    @Test
+    public void delete_other_user_admin(){
+        User adminUser = new User("Admin", "1234", "Edit Users", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.delete_user(otherUser);
+        // pass
     }
 
     // Delete other user, without admin, fail
     @Test
-    public static void delete_other_user_without_admin(){
-
-    }
-
-    // Log out
-    @Test
-    public static void log_out_test(){
-
+    public void delete_other_user_without_admin(){
+        User adminUser = new User("Admin", "1234", "Edit Billboards");
+        User otherUser = new User("Other", "5678", "Edit Billboards");
+        userManager admin = new userManager(adminUser, otherUser);
+        admin.delete_user(otherUser);
+        // fail
     }
 }
 
--- src/userManager.java
+++ src/userManager.java
@@ -9,11 +9,20 @@
 public class userManager
 {
     /**
-     * Method for setting user permissions
+     * Method for adding user permissions
      * @param user (object)
      * @param permissions (hashset)
      */
-    public void set_user_permissions(){
+    public void add_user_permissions(){
+
+    }
+
+    /**
+     * Method for removing user permissions
+     * @param user (object)
+     * @param permissions (hashset)
+     */
+    public void remove_user_permissions(){
 
     }
 
@@ -23,6 +32,14 @@ public class userManager
      * @param password (string)
      */
     public void set_user_password(){
+
+    }
+
+    /**
+     * Method for getting user password
+     * @param user (object)
+     */
+    public void get_user_password(){
 
     }
 
@@ -41,46 +58,3 @@ public class userManager
 
     }
 }
-
-/*      Comments regarding several methods of user manager
-
-        set_user_permissions
-        - adds permission to hashset of permissions
-        - when editting some permissions with already be there, so don't double up
-        * user cant remove edit user from themselves
-
-
-        set_user_password
-        - user must provide username and new password
-        - clicks apply, returns acknowledgement
-        * if change user password not check for admin, but if not check for it
-
-        delete_user
-        - user must put in username of user
-        - check for admin priviledgs, and valid username
-        * note some billboards will no longer have owners
-        * user can't remove themselves
-
-        log_out
-        - clicks button to log_out
-        - server side stuff that i have no idea how to do
-
-        tests
-
-        set_user_permissions
-        - remove a/multiple permission/s
-        - add a/multiple permission/s
-        - check that user with existing permission doesn't double up on permission
-        - check that user with edit user can't remove it from themselves
-        - check that user without edit_user permission can't do anything
-
-        set_user_password
-        - set own password, with and without admin
-        - set other password, with and without admin
-
-        delete_user
-        - delete own user, error
-        - delete other user, with and without admin
-
-        log_out
-        - with and without admin, no issues*/
