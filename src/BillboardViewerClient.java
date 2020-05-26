2e0ab9f2d568f9d4959643324912a34ea54df9de
emilychang <changemily5@gmail.com>
2020-05-26 23:26:15 +1000

    BillboardViewerClient javadoc updated

--- src/BillboardViewerClient.java
+++ src/BillboardViewerClient.java
@@ -5,26 +5,27 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.awt.*;
 import java.io.*;
 import java.net.Socket;
-import java.security.NoSuchAlgorithmException;
-import java.sql.Time;
 import java.util.Properties;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import java.awt.*;
-
-import static java.awt.Frame.MAXIMIZED_BOTH;
-
+/**
+ * Control Panel class
+ * Class contains methods for connecting to, sending a request for the name of the
+ * currently displayed billboard and retrieving the name of the
+ * currently displayed billboard from the server over a port,
+ * @author Emily Chang
+ * @version - under development
+ */
 public class BillboardViewerClient {
     private static boolean firstTime = true;
-    private static String[] billboardArray = {"./1.xml", "./2.xml", "./3.xml", "./4.xml", "./5.xml"}; // only for test
-    private static int arrayNo = 0; // only for test
     private static JFrame billboardGUI = new JFrame();
     private static JPanel billboardPanel = new JPanel();
+
     /**
-     * Sends requests to Server
+     * Sends request to server for currently displayed billboard
      */
-    public static void Run_Client(){
+    public static void runClient(){
         Properties props = new Properties();
         FileInputStream fileIn = null;
         int portNumber;
@@ -33,6 +34,7 @@ public class BillboardViewerClient {
                 billboardGUI.setUndecorated(true);
                 firstTime = false;
             }
+
             //read from network props file
             fileIn = new FileInputStream("./network.props");
             props.load(fileIn);
@@ -59,13 +61,13 @@ public class BillboardViewerClient {
             oos.flush();
 
             //retrieve name of currently displayed billboard
-            String billboard_name = ois.readObject().toString();
+            String billboardName = ois.readObject().toString();
 
             //print what was received from server
-            System.out.println("billboard being displayed: "+ billboard_name);
+            System.out.println("billboard being displayed: "+ billboardName);
 
             //get xml file name for billboard
-            File file = new File("./"+billboard_name+".xml");
+            File file = new File("./"+billboardName+".xml");
 
             //display billboard on viewer
             ViewerGUI(billboardGUI, billboardPanel, file);
@@ -93,6 +95,15 @@ public class BillboardViewerClient {
         }
     }
 
+    /**
+     *
+     * @param billboardGUI
+     * @param billboardPanel
+     * @param file
+     * @throws IOException
+     * @throws SAXException
+     * @throws ParserConfigurationException
+     */
     public static void ViewerGUI(JFrame billboardGUI, JPanel billboardPanel, File file) throws IOException, SAXException, ParserConfigurationException {
         if(billboardPanel != null){
             billboardGUI.remove(billboardPanel);
@@ -109,17 +120,19 @@ public class BillboardViewerClient {
         billboardGUI.add(billboardPanel);
     }
 
-    public static void main(String args[]) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IOException, IllegalAccessException {
-        //SwingUtilities.invokeLater(new ControlPanelGUI());
-
+    /**
+     * Runs viewer request to server every 15 seconds, displays current billboard on viewer GUI
+     * @param args
+     */
+    public static void main(String args[]){
         //create new timer
         Timer timer = new Timer();
 
-        //create timer task that requests current billboard from server
+        //create timer task that requests current billboard from server and displays it on the viewer
         class runViewer extends TimerTask {
             public void run() {
                 try {
-                    Run_Client();
+                    runClient();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -128,7 +141,5 @@ public class BillboardViewerClient {
 
         //request current billboard every 15 seconds & update viewer
         timer.schedule(new runViewer(), 0, 15000);
-
     }
-
 }
