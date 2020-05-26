95f08318a5dfa8b0d2c727546d88640c1ce97c31
emilychang <changemily5@gmail.com>
2020-05-26 20:16:11 +1000

    Unit tests written for Schedul_info class

--- src/ControlPanelClient.java
+++ src/ControlPanelClient.java
@@ -312,7 +312,7 @@ public class ControlPanelClient {
 
         String time = LocalDateTime.now().plus(Duration.ofMinutes(1)).toString();
 
-        String [] user_inputs1 = {"Schedule Billboard","3",time, "1", "60"};
+        String [] user_inputs1 = {"Schedule Billboard","3",time, "1", "2"};
         String [] user_inputs2 = {"Schedule Billboard","2","2021-05-10T10:00:00.00", "2", "0"};
         String [] user_inputs3 = {"Schedule Billboard","1","2021-05-10T10:00:00.00", "1", "0"};
 
---
+++ src/TestScheduleInfo.java
@@ -0,0 +1,94 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestScheduleInfo {
+
+    Schedule_Info schedule_info;
+    final int minutes_inDay = 1440;
+    final int minutes_inHour = 60;
+
+    // Test 1: Constructing a Schedule info object with scheduled time
+    @BeforeEach
+    @Test
+    public void setUpBillboard() throws Exception {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(5),
+                5, "emily");
+    }
+
+    @Test
+    // Test 2: construct schedule info object that recurs daily
+    public void recurrence_day() throws Exception {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(5),
+                minutes_inDay, "emily");
+
+        assertEquals(minutes_inDay, schedule_info.Recurrence_delay);
+    }
+
+    @Test
+    // Test 3: construct schedule info object that recurs hourly
+    public void recurrence_hour() throws Exception {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(5),
+                minutes_inHour, "emily");
+
+        assertEquals(minutes_inHour, schedule_info.Recurrence_delay);
+    }
+
+    @Test
+    // Test 4: construct schedule info object that recurs every 3 mins
+    public void recurrence_min() throws Exception {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(1),
+                3, "emily");
+
+        assertEquals(3, schedule_info.Recurrence_delay);
+    }
+
+    @Test
+    // Test 5: construct schedule info object with invalid recurrence delay - invalid option, larger than 60 mins and not an hr or day
+    public void invalid_recurrence() throws Exception {
+        assertThrows(Exception.class,() -> {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(5),
+                61, "emily");
+        });
+    }
+
+    @Test
+    // Test 6: construct schedule info object with invalid recurrence delay - smaller than duration
+    public void invalid_recurrence2() throws Exception {
+        assertThrows(Exception.class,() -> {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(5),
+                2, "emily");
+        });
+    }
+
+    @Test
+    //Test 7: construct schedule info object with valid duration - Negative duration
+    public void Valid_duration() throws Exception {
+        schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(5),
+                2, "emily");
+        assertEquals(Duration.ofMinutes(5), schedule_info.duration);
+    }
+
+    @Test
+    //Test 8: construct schedule info object with invalid duration - Negative duration
+    public void Neg_duration() throws Exception {
+        assertThrows(Exception.class,() -> {
+            schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(-5),
+                    2, "emily");
+        });
+    }
+
+    @Test
+    //Test 9: construct schedule info object with invalid duration - Zero duration
+    public void Zer_duration() throws Exception {
+        assertThrows(Exception.class,() -> {
+            schedule_info = new Schedule_Info(LocalDateTime.parse("2021-05-26T10:00:00"), Duration.ofMinutes(0),
+                    2, "emily");
+        });
+    }
+}
